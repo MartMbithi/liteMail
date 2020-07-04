57fa3d5e8fa01c2b40d24d9b3f262dd2a86cbfff
martin <martdevelopers254@gmail.com>
2020-07-04 17:58:03 +0300

    Implemented home page front end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,25 +3,36 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'liteMail') }}</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&amp;display=swap" rel="stylesheet">
     <link href="{{ url('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ url('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ url('assets/css/authentication/form-1.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END GLOBAL MANDATORY STYLES -->
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/forms/theme-checkbox-radio.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/forms/switches.css') }}">
+    <link href="{{ url('assets/css/forms/theme-checkbox-radio.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('assets/css/forms/switches.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('plugins/editors/quill/quill.snow.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('assets/css/apps/mailbox.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('plugins/sweetalerts/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('plugins/sweetalerts/sweetalert.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('plugins/notification/snackbar/snackbar.min.css') }}" rel="stylesheet" type="text/css" />
+        
+    <!-- Scripts -->
+    <script src="{{ url('plugins/sweetalerts/promise-polyfill.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
 </head>
 <body>
     <div id="app">
+        <!--
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -30,16 +41,12 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -57,8 +64,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -72,16 +79,39 @@
                 </div>
             </div>
         </nav>
+        -->
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+    <!--footer-->
+    <div class="footer-wrapper">
+        <div class="footer-section f-section-1">
+            <p class="">{{ __('Copyright © 2020 liteMail') }} </a>, {{ __('All rights reserved. A ')}}<a href="https://martdev.info/">{{ __('MartDevelopers')}}</a>{{ __('Production')}}</p>
+        </div>
+        <div class="footer-section f-section-2">
+            <p class="">{{ __('Crafted With') }} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></p>
+        </div>
+        </div>
+    </div>
+    <!-- ./ footer-->
 </body>
     <!--Scripts -->
     <script src="{{ url('assets/js/libs/jquery-3.1.1.min.js') }}"></script>
     <script src="{{ url('bootstrap/js/popper.min.js') }}"></script>
     <script src="{{ url('bootstrap/js/bootstrap.min.js') }}"></script>
-    <!-- END GLOBAL MANDATORY SCRIPTS -->
-    <script src="{{ url('assets/js/authentication/form-1.js') }}"></script>
+    <script src="{{ url('assets/js/app.js') }}"></script>
+    
+    <script>
+        $(document).ready(function() {
+            App.init();
+        });
+    </script>
+    <script src="{{ url('assets/js/custom.js') }}"></script>
+    <script src="{{ url('assets/js/ie11fix/fn.fix-padStart.js') }}"></script>
+    <script src="{{ url('plugins/editors/quill/quill.js') }}"></script>
+    <script src="{{ url('plugins/sweetalerts/sweetalert2.min.js') }}"></script>
+    <script src="{{ url('plugins/notification/snackbar/snackbar.min.js') }}"></script>
+    <script src="{{ url('assets/js/apps/custom-mailbox.js') }}"></script>
 </html>

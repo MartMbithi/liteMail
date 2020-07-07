34fa458f282ab5880cb5db6fad78f3fd64f0eb94
martin <martdevelopers254@gmail.com>
2020-07-07 14:29:47 +0300

    Implemented profile route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/profile', 'HomeController@profile')->name('profile');
+
+Route::get('/profile', function(){
+    return view('profile');
+});
